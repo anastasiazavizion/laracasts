db3faac2130e45401ec3cfc4eb41dbde94e89f35
Anastasia Zavizion <a.zavizion.a@gmail.com>
2026-08-12 12:55:41 +0200

    Pass data to blade

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<x-layout title="About">
+ <h1>About Page</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['name'=>request('name','Guest')]);
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
